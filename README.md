e4526d0e404c9696269d5e0194f60e9a328cb8e3
snowcharm <619022098@qq.com>
2022-06-19 16:23:59 +0800

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2022-Spring-C-and-Cpp-Project-Matrix
-The project for C and C++ Programming in 2022 spring
--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm -rf CMakeFiles
-rm -rf .vscode
-rm -f cmake_install.cmake
-rm -f CMakeCache.txt
-rm -f Makefile
-rm -f matrix
-rm -f a.out
-rm -f main
